6e11bda3b58564275574f130bec1e6f25f5cfd98
Louis Jarraud <diamondsbattle@tutanota.com>
2021-03-09 19:03:28 +0100

    Added levels

---
+++ lvls/lvl0.py
@@ -0,0 +1,9 @@
+from prefabs.level import Level
+
+
+class Level0(Level):
+    def __init__(self):
+        super().__init__(
+            name='level0',
+            description='prologue',
+        )
