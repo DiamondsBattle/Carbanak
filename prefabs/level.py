d13a6788e472acc307cbaab09c1bf0cb6f68d946
Louis Jarraud <diamondsbattle@tutanota.com>
2021-03-10 16:11:07 +0100

    Added next attr to Level

--- prefabs/level.py
+++ prefabs/level.py
@@ -1,4 +1,14 @@
+from ursina import *
+
+
 class Level:
-    def __init__(self, name, description):
+    def __init__(self, name, description, next=None):
         self.name = name
         self.description = description
+        self.next = next
+
+        self.entities = {}
+
+    def unLoad(self):
+        for i, e in self.entities:
+            destroy(e)
