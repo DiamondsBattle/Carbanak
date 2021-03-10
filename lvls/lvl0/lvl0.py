352f4762085f1a0bbfeae5789517afcb41599ac8
Louis Jarraud <diamondsbattle@tutanota.com>
2021-03-10 18:04:00 +0100

    Added text script

--- lvls/lvl0/lvl0.py
+++ lvls/lvl0/lvl0.py
@@ -2,8 +2,9 @@ from prefabs.level import Level
 
 from prefabs.screen import Screen
 from prefabs.halo import Halo
+from prefabs.script import Script
 
-from ursina import invoke
+from ursina import invoke, window, Vec2
 from json import loads
 
 
@@ -16,14 +17,22 @@ class Level0(Level):
             **kwargs,
         )
         with open('lvls/lvl0/text.json', 'r') as f:
-            self.text = loads(f.read())
+            data = loads(f.read())
+        self.script = data[self.lang]
 
         self.entities = {
             'screen': Screen(),
             'halo': Halo(),
+            'script': Script(
+                script=self.script,
+                position=Vec2(0, -.5),
+                origin=(0, -.5)
+            )
         }
 
         for i in self.entities:
             self.entities[i].enabled = False
+
+        self.entities['script'].enabled = True
 
         invoke(setattr, self.entities['screen'], 'enabled', True, delay=3)
